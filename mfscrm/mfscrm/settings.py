2a361c1b917ac376a628c041847dfbd5f82d9f03
madhumitha27 <aparabm@gmail.com>
2020-02-15 13:26:45 -0600

    my commit

--- mfscrm/mfscrm/settings.py
+++ mfscrm/mfscrm/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'zxs(f(@lnu2(htv^xqwlp)fip^$rtsdjc-n3_)k*h0(!%%0u=d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -127,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = '/home'
 LOGOUT_REDIRECT_URL = '/home'
